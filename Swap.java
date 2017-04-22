d132bc3ed52d6d6e42df888ab2c69c47d57d926a
arjun-github <arjunsingh.gcet@gmail.com>
2017-04-22 11:56:50 +0530

    swap program
    two variable value swap without using third variable

---
+++ Swap.java
@@ -0,0 +1,17 @@
+
+public class Swap {
+
+	static int a=10;
+	static int b=20;
+	public static void main(String[] args) {
+		
+	a=a+b;
+	b=a-b;
+	a=a-b;
+	System.out.println("a="+a);
+	System.out.println("b="+b);
+	
+
+	}
+
+}
